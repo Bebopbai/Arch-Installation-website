215de1b378c0a957749b1d38dbffb920397019fb
Talha yousaf <tayou@Talhas-MacBook-Pro.local>
2023-12-09 20:46:18 -0500

    added functionality to complete button so it changes button and checkmark colour

--- Pages/btnfunction.js
+++ Pages/btnfunction.js
@@ -1,10 +1,84 @@
 
-document.querySelector('.ch-1').style.backgroundColor = localStorage.getItem('background-color');
+
+//document.querySelector('button').style.backgroundColor = localStorage.getbutton(storedValue);
+
+
+
+var buttons = document.querySelectorAll("#button1menu");
+
+// Check the stored value in sessionStorage
+var storedColor = sessionStorage.getItem("key2");
+
+// Check if the stored color is "lightgreen" and show an alert
+var checkmarks = document.querySelectorAll(".check-mark-icon")
+
+
+if (storedColor === "#acf799") {
+    checkmarks.forEach(checkmark => {
+        checkmarks[0].style.filter = "invert(76%) sepia(9%) saturate(1373%) hue-rotate(326deg) brightness(103%) contrast(84%)";
+        console.log("Setting width for checkmark");
+    });
+}
+
+
+
+
+buttons.forEach((button) => {
+    let btnToggle = true;
+    // Set the initial background color based on the stored value
+    button.style.backgroundColor = storedColor;
+
+    button.addEventListener('click', () => {
+        if (btnToggle) {
+            button.style.backgroundColor = "#acf799";
+            sessionStorage.setItem("key2", "#acf799");
+            btnToggle = false;
+        } else {
+            button.style.backgroundColor = null; // Set to null to remove inline style
+            sessionStorage.setItem("key2", "null");
+            btnToggle = true;
+        }
+        
+    });
+});
+
+
+/*
+buttons.forEach((button) => {
+    var btnToggle = true
+    
+    button.addEventListener('click', () => {
+        
+        if (btnToggle){
+            button.style.backgroundColor = "pink";
+            //var bgcolor = button.id.style.backgroundColor;
+            //localStorage.setbutton("storedValue", bgcolor);
+            btnToggle = false
+        }
+        else{
+            button.style.backgroundColor = "lightblue";
+            //var bgcolor2 = button.id.style.backgroundColor;
+            //localStorage.setbutton("storedValue", bgcolor2);
+            btnToggle = true
+        }
+    })
+})
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 
 /*
-const getColorData = localStorage.getItem('background-color');
+const getColorData = localStorage.getbutton('background-color');
 function testCase(selector){
     try {
         // Attempt to retrieve the background-color from localStorage
@@ -18,7 +92,7 @@ function testCase(selector){
 
         // Rest of your code...
     } catch (error) {
-        // Log any errors that occur during the getItem operation
+        // Log any errors that occur during the getbutton operation
         console.error('Error retrieving background-color:', error);
 
         // Optionally, provide a default background color or handle the error in another way
@@ -27,20 +101,20 @@ function testCase(selector){
 
 }
 
-*/
+
 var btnToggle = true;
 function btnclicked(path){
     
        
     // Use the retrieved color or provide a default color if it's not set
-    // console.log('Before getItem:', localStorage.getItem('background-color'));
+    // console.log('Before getbutton:', localStorage.getbutton('background-color'));
 
     if (btnToggle){
         
         
         document.querySelector(path).style.backgroundColor = "#A7C68C";
         var bgcolor = document.querySelector(path).style.backgroundColor;
-        localStorage.setItem("background-color", bgcolor);
+        localStorage.setbutton("background-color", bgcolor);
         btnToggle = false;
 
     } 
@@ -49,7 +123,7 @@ function btnclicked(path){
         
         document.querySelector(path).style.backgroundColor = "#EAB2A0";
         var bgcolor = document.querySelector(path).style.backgroundColor;
-        localStorage.setItem("background-color", bgcolor);
+        localStorage.setbutton("background-color", bgcolor);
         btnToggle = true;
     }
     
@@ -87,3 +161,5 @@ function checkColor() {
 
 setInterval(checkColor, 1000);
 */
+
+
